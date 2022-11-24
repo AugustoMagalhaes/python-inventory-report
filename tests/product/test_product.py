a941b793327cfcc8292efeca9df3a2228f1b9d5a
AugustoMagalhaes <augustommg@gmail.com>
2022-11-24 20:52:50 -0300

    feat: implementa test_cria_produto.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    cdb = (Product(
+      1,
+      "CDB",
+      "Banco do Brasil",
+      "24/11/2022",
+      "23/05/2024",
+      "CDB00001C",
+      "De preferências em um serviço de nuvem com três zonas " +
+      "de disponibilidade"
+    ))
+
+    assert cdb.id == 1
+    assert cdb.id != 2
+    assert cdb.nome_do_produto == "CDB"
+    assert cdb.data_de_fabricacao == "24/11/2022"
+    assert cdb.data_de_validade == "23/05/2024"
+    assert cdb.numero_de_serie == "CDB00001C"
+    assert cdb.instrucoes_de_armazenamento == (
+     "De preferências em um serviço de nuvem com três zonas de disponibilidade"
+     )
