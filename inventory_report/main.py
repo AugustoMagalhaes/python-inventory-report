bae5d362e4a8735ebba1747b0a0dc9d39bf16685
AugustoMagalhaes <augustommg@gmail.com>
2022-11-27 23:35:34 -0300

    feat: cria funcoes get_importer e get_args em main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,26 @@
+import sys
+
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
+def get_importer(importer):
+    importer_map = {
+        'csv': CsvImporter,
+        'json': JsonImporter,
+        'xml': XmlImporter,
+    }
+    try:
+        return importer_map[importer]
+    except KeyError:
+        print(f"{importer} não encontrado")
+
+
+def get_args():
+    return sys.argv[:1]
+
+
 def main():
-    pass
+    lista_args = get_args()
+
